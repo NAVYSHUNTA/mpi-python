defd584b9382e62d175885627f8748fb863af021
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-06-10 03:06:36 +0000

    素数の並列処理のバグ修正（処理に不要な重複があった）

--- src/math/count_prime_parallel.py
+++ src/math/count_prime_parallel.py
@@ -30,18 +30,6 @@ def main():
         comm.send(count, dest = 0, tag = rank)
 
     if rank == 0:
-        for i in range(1, size):
-            base = n // (size - 1)
-            comm.send([base * (i - 1), min(n, base * i)], dest = i, tag = i)
-    else:
-        left, right = comm.recv(source = 0, tag = rank)
-        print(f"わたしはスタッフ {rank} で、{left} 以上 {right - 1} 以下の素数を数えますね！")
-        count = 0
-        for i in range(left, right):
-            count += is_prime(i) # Python では True の場合は 1, False の場合は 0 として扱われる
-        comm.send(count, dest = 0, tag = rank)
-
-    if rank == 0:
         time.sleep(0.5) # 他のスタッフが計算を終えるのを待つためにわざと待機
         total_count = 0
         for i in range(1, size):
