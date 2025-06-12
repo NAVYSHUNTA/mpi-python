2753a6b287e71495c836ce7191e4474cd267bc32
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-06-12 14:44:17 +0000

    リファクタリング

--- src/math/count_prime_parallel.py
+++ src/math/count_prime_parallel.py
@@ -15,36 +15,39 @@ def is_prime(target):
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    size = comm.Get_size()
+    all_size = comm.Get_size()
+    work_size = all_size - 1 # 並列度（指示を出すスタッフを除く）
     n = 20000 # 今後、ユーザーからの入力に変更する？（TODO）
 
     if rank == 0:
-        for i in range(1, size):
-            base = math.ceil(n / (size - 1) ** 2)
-            one_step_base = base * (size - 1)
-            for j in range(1, size):
+        for i in range(1, all_size):
+            base = math.ceil(n / work_size ** 2)
+            one_step_base = base * work_size
+            for j in range(1, all_size):
                 s = (i - 1) * one_step_base
                 comm.send([s + base * (j - 1) + 1, min(n + 1, s + base * j + 1)], dest = j, tag = i)
     else:
-        for i in range(1, size):
+        for i in range(1, all_size):
             left, right = comm.recv(source = 0, tag = i)
+
+            # 無効な範囲の場合はカウントせずに終了
             count = 0
             if left >= right:
                 comm.send(count, dest = 0, tag = i)
                 continue
 
-            print(f"わたしはスタッフ {rank} で、{left} 以上 {right - 1} 以下の素数を数えますね！")
+            print(f"わたしはスタッフ {rank} で、{left} 以上 {right - 1} 以下の素数を数えますね！") # TODO: ここはスパコンで時間計測する前に削除する
             for num in range(left, right):
                 count += is_prime(num) # Python では True の場合 1, False の場合 0 として扱われる
             comm.send(count, dest = 0, tag = i)
 
     if rank == 0:
-        time.sleep(0.5) # 他のスタッフが計算を終えるのを待つためにわざと待機
-        total_count = 0
-        for i in range(1, size):
-            for j in range(1, size):
-                total_count += comm.recv(source = i, tag = j)
-        print(f"1 から {n} までの素数の個数は {total_count} です")
+        time.sleep(0.5) # 他のスタッフが計算を終えるのを待つためにわざと待機（TODO: ここはスパコンで時間計測する前に削除する）
+        total_prime_count = 0
+        for i in range(1, all_size):
+            for j in range(1, all_size):
+                total_prime_count += comm.recv(source = i, tag = j)
+        print(f"1 から {n} までの素数の個数は {total_prime_count} です")
 
 if __name__ == "__main__":
     main()
