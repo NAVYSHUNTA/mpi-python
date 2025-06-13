3b0de6afe4187c4d40b1099b1ffede17e458edf1
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-06-13 18:35:21 +0000

    リファクタリング

--- src/math/count_prime.py
+++ src/math/count_prime.py
@@ -4,22 +4,21 @@
 def is_prime(target):
     if target < 2:
         return False
-    for i in range(2, target):
-        if target % i == 0:
+    for div in range(2, target):
+        if target % div == 0:
             return False
     return True
 
 # 1 から n までの素数の個数を数え上げ
 def count_prime(n):
     count = 0
-    for i in range(1, n + 1):
-        count += is_prime(i) # Python では True の場合 1, False の場合 0 として扱われる
+    for num in range(1, n + 1):
+        count += is_prime(num) # Python では True の場合 1, False の場合 0 として扱われる
     return count
 
 def main():
     n = int(input()) # 入力を受け取って整数に変換
     print(n, count_prime(n)) # (入力値, 答え, 実行時間) の形式にする（TODO）
-
 
 if __name__ == "__main__":
     main()
--- src/math/count_prime_parallel.py
+++ src/math/count_prime_parallel.py
@@ -13,39 +13,39 @@ WORK_MEMBER_SIZE = ALL_MEMBER_SIZE - 1 # 並列度（メンバーに指示を出
 def is_prime(target):
     if target < 2:
         return False
-    for i in range(2, target):
-        if target % i == 0:
+    for div in range(2, target):
+        if target % div == 0:
             return False
     return True
 
 # 1 から n までの素数の個数を数え上げ（計算の指示）
 def calc_order(n):
-    if rank == 0:
-        for i in range(1, ALL_MEMBER_SIZE):
+    if rank == LEADER_RANK:
+        for repeat in range(1, ALL_MEMBER_SIZE):
             base = math.ceil(n / WORK_MEMBER_SIZE ** 2)
             one_step_base = base * WORK_MEMBER_SIZE
-            for j in range(1, ALL_MEMBER_SIZE):
-                s = (i - 1) * one_step_base
-                COMM.send([s + base * (j - 1) + 1, min(n + 1, s + base * j + 1)], dest = j, tag = i)
+            for member in range(1, ALL_MEMBER_SIZE):
+                s = (repeat - 1) * one_step_base
+                COMM.send([s + base * (member - 1) + 1, min(n + 1, s + base * member + 1)], dest = member, tag = repeat)
 
 # 1 から n までの素数の個数を数え上げ（指示された内容の計算）
 def count_prime():
     if rank == LEADER_RANK:
         pass # nothing
     else:
-        for i in range(1, ALL_MEMBER_SIZE):
-            left, right = COMM.recv(source = 0, tag = i)
+        for repeat in range(1, ALL_MEMBER_SIZE):
+            left, right = COMM.recv(source = LEADER_RANK, tag = repeat)
 
             # 無効な範囲の場合はカウントせずに終了
             count = 0
             if left >= right:
-                COMM.send(count, dest = 0, tag = i)
+                COMM.send(count, dest = LEADER_RANK, tag = repeat)
                 continue
 
             print(f"わたしはメンバー {rank} で、{left} 以上 {right - 1} 以下の素数を数えますね！") # TODO: この行はスパコンで時間計測する前に削除する
             for num in range(left, right):
                 count += is_prime(num) # Python では True の場合 1, False の場合 0 として扱われる
-            COMM.send(count, dest = 0, tag = i)
+            COMM.send(count, dest = LEADER_RANK, tag = repeat)
 
 def main():
     # ユーザーから受け取った入力をもとにリーダーがメンバーへ計算を指示する
@@ -60,9 +60,9 @@ def main():
     if rank == LEADER_RANK:
         time.sleep(0.5) # 他のメンバーが計算を終えるのを待つためにわざと待機（TODO: この行はスパコンで時間計測する前に削除する）
         total_prime_count = 0
-        for i in range(1, ALL_MEMBER_SIZE):
-            for j in range(1, ALL_MEMBER_SIZE):
-                total_prime_count += COMM.recv(source = i, tag = j)
+        for member in range(1, ALL_MEMBER_SIZE):
+            for repeat in range(1, ALL_MEMBER_SIZE):
+                total_prime_count += COMM.recv(source = member, tag = repeat)
         print(f"1 から {n} までの素数の個数は {total_prime_count} です")
 
 if __name__ == "__main__":
