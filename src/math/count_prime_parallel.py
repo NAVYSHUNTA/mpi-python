311c23240bb5210b50b7c8d3bd61370892686961
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-06-21 01:32:17 +0900

    素数の並列処理を高速化した

--- src/math/count_prime_parallel.py
+++ src/math/count_prime_parallel.py
@@ -6,8 +6,7 @@ import math
 COMM = MPI.COMM_WORLD # 全てのプロセスで共有されるコミュニケータ
 rank = COMM.Get_rank() # 自プロセスのランク番号
 LEADER_RANK = 0 # リーダーの rank 番号
-ALL_MEMBER_SIZE = COMM.Get_size() # 並列度（厳密にはそのコミュニケータに所属するプロセスの総数）
-WORK_MEMBER_SIZE = ALL_MEMBER_SIZE - 1 # 並列度（メンバーに指示を出すリーダー自身を除く）
+SIZE = COMM.Get_size() # 並列度（厳密にはそのコミュニケータに所属するプロセスの総数）
 
 # 素数判定
 # 入力値を N としたとき、この素数判定アルゴリズムの時間計算量は O(N)
@@ -20,35 +19,6 @@ def is_prime(target):
             return False # 割り切れる場合は素数ではないので False を返す
     return True
 
-# 1 から n までの素数の個数を数え上げ（計算の指示）
-def calc_order(n):
-    if rank == LEADER_RANK:
-        for repeat in range(1, ALL_MEMBER_SIZE):
-            base = math.ceil(n / WORK_MEMBER_SIZE ** 2)
-            one_step_base = base * WORK_MEMBER_SIZE
-            for member in range(1, ALL_MEMBER_SIZE):
-                s = (repeat - 1) * one_step_base
-                COMM.send([s + base * (member - 1) + 1, min(n + 1, s + base * member + 1)], dest = member, tag = repeat)
-
-# 1 から n までの素数の個数を数え上げ（指示された内容の計算）
-def count_prime():
-    if rank == LEADER_RANK:
-        pass # nothing
-    else:
-        for repeat in range(1, ALL_MEMBER_SIZE):
-            left, right = COMM.recv(source = LEADER_RANK, tag = repeat)
-
-            # 無効な範囲の場合はカウントせずに終了
-            count = 0
-            if left >= right:
-                COMM.send(count, dest = LEADER_RANK, tag = repeat)
-                continue
-
-            print(f"わたしはメンバー {rank} で、{left} 以上 {right - 1} 以下の素数を数えますね！", flush = True) # TODO: この行はスパコンで時間計測する前に削除する
-            for num in range(left, right):
-                count += is_prime(num) # int 型と bool 型を足したとき、Python では True, False がそれぞれ 1, 0 として扱われるのでこれを利用してカウントする
-            COMM.send(count, dest = LEADER_RANK, tag = repeat)
-
 # 小数点以下を切り捨てたミリ秒単位の実行時間（実時間）を返す
 def get_total_time_ms_floor(start_time):
     end_time = time.perf_counter()
@@ -58,24 +28,36 @@ def get_total_time_ms_floor(start_time):
     return total_time_ms_floor
 
 def main():
+    n = None
     start_time = None
-    # ユーザーから受け取った入力をもとにリーダーがメンバーへ計算を指示する
     if rank == LEADER_RANK:
         n = int(input()) # 入力を受け取って整数に変換
         start_time = time.perf_counter()
-        calc_order(n) # メンバーに計算の指示を出す
 
-    # 各メンバーが計算を行う
-    count_prime()
+    n = COMM.bcast(n, root = LEADER_RANK) # 全てのメンバーに n を配布する
+    one_step_range = math.ceil(n / min(n, SIZE ** 2)) # 各プロセスが調査する整数の範囲の幅
+    count_prime_self = 0 # 自プロセスがカウントした素数の個数
 
-    # リーダーが計算結果を集計する
-    COMM.Barrier() # 全てのスレッドを同期させる（待機）
+    # 素数の個数を数え上げる
+    for i in range(SIZE ** 2):
+        if i % SIZE != rank:
+            continue # 他のプロセスの担当箇所は無視する
+        start_num = one_step_range * i + 1
+        end_num = min(n, one_step_range * (i + 1))
+
+        if start_num > n:
+            break # どのプロセスでも n より大きい整数は扱わないので終了する
+
+        # 自プロセスは start_num 以上 end_num 以下の整数に含まれる素数の個数を数える
+        print(f"わたしはメンバー {rank} で、{start_num} 以上 {end_num} 以下の整数に含まれる素数の個数を数えますね！", flush = True) # TODO: この行はスパコンで時間計測する前に削除する
+        for num in range(start_num, end_num + 1):
+            count_prime_self += is_prime(num)
+
+    # 各プロセスが数え上げた素数の個数の総和をリーダーに渡す
+    total_count_prime = COMM.reduce(count_prime_self, op = MPI.SUM, root = LEADER_RANK)
+
+    # 計算結果の出力
     if rank == LEADER_RANK:
-        total_count_prime = 0
-        for member in range(1, ALL_MEMBER_SIZE):
-            for repeat in range(1, ALL_MEMBER_SIZE):
-                total_count_prime += COMM.recv(source = member, tag = repeat)
-
         total_time_ms_floor = get_total_time_ms_floor(start_time)
         print(n, total_count_prime, total_time_ms_floor) # 入力値, 1 から n までの素数の個数, 実行時間（ミリ秒）
 
